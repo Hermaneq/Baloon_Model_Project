1381c72c3cae120ac870adbeb927ad7e8fba7d86
Bartosz Hermanowski <b-hermanowski@hotmail.com>
2023-11-13 17:06:14 +0100

    All usages included

--- main.go
+++ main.go
@@ -17,12 +17,16 @@ func main() {
 	// Initializing data
 	var radius float64 = 6
 	var surface float64 = 4 * radius * radius * math.Pi
+	var startSurface float64 = surface
+	var tension float64 = (0.5 / startSurface) * (1 - (math.Pow(startSurface, 3) / (math.Pow(surface, 3)))) * 1000
 	var volume float64 = 200
 	var weight float64 = 0.241
+	var balloonTension []float64
 	var balloonVolume []float64
 	var balloonWeight []float64
 	var balloonRadius []float64
 	var balloonSurface []float64
+	balloonTension = append(balloonTension, tension)
 	balloonSurface = append(balloonSurface, surface)
 	balloonVolume = append(balloonVolume, 200)
 	balloonWeight = append(balloonWeight, 0)
@@ -35,27 +39,29 @@ func main() {
 	w := plot.New()
 	d := plot.New()
 	q := plot.New()
+	t := plot.New()
 
 	// Balloon pumping simulation
-	balloonVolume, balloonWeight, balloonRadius, balloonSurface, time = pumpBalloon(volume, time, weight, radius, surface, balloonSurface, balloonVolume, balloonWeight, balloonRadius)
+	balloonTension, balloonVolume, balloonWeight, balloonRadius, balloonSurface, time = pumpBalloon(tension, startSurface, volume, time, weight, radius, surface, balloonSurface, balloonVolume, balloonWeight, balloonRadius, balloonTension)
 
 	// Creating plots data
 	pts := make(plotter.XYs, time)
 	wts := make(plotter.XYs, time)
 	dts := make(plotter.XYs, time)
 	qts := make(plotter.XYs, time)
+	tts := make(plotter.XYs, time)
 
-	addData(pts, wts, dts, qts, time, balloonVolume, balloonWeight, balloonRadius, balloonSurface)
-	createLines(pts, p, wts, w, dts, d, qts, q)
-	setPlotDetails(p, w, d, q)
+	addData(pts, wts, dts, qts, tts, time, balloonVolume, balloonWeight, balloonRadius, balloonSurface, balloonTension)
+	createLines(pts, p, wts, w, dts, d, qts, q, tts, t)
+	setPlotDetails(p, w, d, q, t)
 
 	// Saving plots to the files
 
-	savePlots(p, w, d, q)
+	savePlots(p, w, d, q, t)
 
 }
 
-func createLines(pts plotter.XYs, p *plot.Plot, wts plotter.XYs, w *plot.Plot, dts plotter.XYs, d *plot.Plot, qts plotter.XYs, q *plot.Plot) {
+func createLines(pts plotter.XYs, p *plot.Plot, wts plotter.XYs, w *plot.Plot, dts plotter.XYs, d *plot.Plot, qts plotter.XYs, q *plot.Plot, tts plotter.XYs, t *plot.Plot) {
 	line, err := plotter.NewLine(pts)
 	if err != nil {
 		log.Fatal(err)
@@ -76,9 +82,14 @@ func createLines(pts plotter.XYs, p *plot.Plot, wts plotter.XYs, w *plot.Plot, d
 		log.Fatal(qErr)
 	}
 	q.Add(qLine)
+	tLine, tErr := plotter.NewLine(tts)
+	if tErr != nil {
+		log.Fatal(tErr)
+	}
+	t.Add(tLine)
 }
 
-func pumpBalloon(volume float64, time int, weight float64, radius float64, surface float64, ballonSurface []float64, balloonVolume []float64, balloonWeight []float64, balloonRadius []float64) ([]float64, []float64, []float64, []float64, int) {
+func pumpBalloon(tension float64, startSurface float64, volume float64, time int, weight float64, radius float64, surface float64, ballonSurface []float64, balloonVolume []float64, balloonWeight []float64, balloonRadius []float64, balloonTension []float64) ([]float64, []float64, []float64, []float64, []float64, int) {
 	for volume < 15000 {
 		time += 1
 
@@ -88,16 +99,19 @@ func pumpBalloon(volume float64, time int, weight float64, radius float64, surfa
 			number := ((3.0 / 4.0) * volume) / math.Pi
 			radius = math.Pow(number, 1.0/3.0)
 			surface = 4 * radius * radius * math.Pi
+			tension = (1 / startSurface) * (1 - (math.Pow(startSurface, 3) / (math.Pow(surface, 3)))) * 1000
 
 			balloonVolume = append(balloonVolume, volume)
 			balloonWeight = append(balloonWeight, weight)
 			balloonRadius = append(balloonRadius, radius)
 			ballonSurface = append(ballonSurface, surface)
+			balloonTension = append(balloonTension, tension)
 		} else {
 			balloonVolume = append(balloonVolume, volume)
 			balloonWeight = append(balloonWeight, weight)
 			balloonRadius = append(balloonRadius, radius)
 			ballonSurface = append(ballonSurface, surface)
+			balloonTension = append(balloonTension, tension)
 		}
 
 	}
@@ -106,12 +120,13 @@ func pumpBalloon(volume float64, time int, weight float64, radius float64, surfa
 		balloonWeight = append(balloonWeight, 0)
 		balloonRadius = append(balloonRadius, 0)
 		ballonSurface = append(ballonSurface, 0)
+		balloonTension = append(balloonTension, 0)
 		time += 1
 	}
-	return balloonVolume, balloonWeight, balloonRadius, ballonSurface, time
+	return balloonTension, balloonVolume, balloonWeight, balloonRadius, ballonSurface, time
 }
 
-func setPlotDetails(p *plot.Plot, w *plot.Plot, d *plot.Plot, q *plot.Plot) {
+func setPlotDetails(p *plot.Plot, w *plot.Plot, d *plot.Plot, q *plot.Plot, t *plot.Plot) {
 	p.Title.Text = "Simulation of the volume of an inflated balloon"
 	p.X.Label.Text = "Time [s]"
 	p.Y.Label.Text = "Balloon volume [mL]"
@@ -120,7 +135,7 @@ func setPlotDetails(p *plot.Plot, w *plot.Plot, d *plot.Plot, q *plot.Plot) {
 
 	w.Title.Text = "Simulation of the weight of an inflated balloon"
 	w.X.Label.Text = "Time [s]"
-	w.Y.Label.Text = "internal weight of the balloon [g]"
+	w.Y.Label.Text = "Internal weight of the balloon [g]"
 	w.X.Min = 0
 	w.Y.Min = 0
 
@@ -130,14 +145,20 @@ func setPlotDetails(p *plot.Plot, w *plot.Plot, d *plot.Plot, q *plot.Plot) {
 	d.X.Min = 0
 	d.Y.Min = 0
 
-	q.Title.Text = "Simulation of the surface of an inflated balloon"
+	q.Title.Text = "Simulation of the surface area of an inflated balloon"
 	q.X.Label.Text = "Time [s]"
-	q.Y.Label.Text = "internal weight of the balloon [cm^2]"
+	q.Y.Label.Text = "Internal weight of the balloon [cm^2]"
 	q.X.Min = 0
 	q.Y.Min = 0
+
+	t.Title.Text = "Simulation of the surface tension of an inflated balloon"
+	t.X.Label.Text = "Time [s]"
+	t.Y.Label.Text = "Surface tension of the balloon"
+	t.X.Min = 0
+	t.Y.Min = 0
 }
 
-func addData(pts plotter.XYs, wts plotter.XYs, dts plotter.XYs, qts plotter.XYs, time int, balloonVolume []float64, balloonWeight []float64, balloonRadius []float64, balloonSurface []float64) {
+func addData(pts plotter.XYs, wts plotter.XYs, dts plotter.XYs, qts plotter.XYs, tts plotter.XYs, time int, balloonVolume []float64, balloonWeight []float64, balloonRadius []float64, balloonSurface []float64, balloonTension []float64) {
 	for i := 0; i < time; i++ {
 		pts[i].X = float64(i)
 		pts[i].Y = balloonVolume[i]
@@ -147,10 +168,12 @@ func addData(pts plotter.XYs, wts plotter.XYs, dts plotter.XYs, qts plotter.XYs,
 		dts[i].Y = balloonRadius[i]
 		qts[i].X = float64(i)
 		qts[i].Y = balloonSurface[i]
+		tts[i].X = float64(i)
+		tts[i].Y = balloonTension[i]
 	}
 }
 
-func savePlots(p *plot.Plot, w *plot.Plot, d *plot.Plot, q *plot.Plot) {
+func savePlots(p *plot.Plot, w *plot.Plot, d *plot.Plot, q *plot.Plot, t *plot.Plot) {
 	if err := p.Save(6*vg.Inch, 4*vg.Inch, "balloonVolume_simulation.png"); err != nil {
 		fmt.Println("Error during saving the plot:", err)
 	} else {
@@ -172,5 +195,11 @@ func savePlots(p *plot.Plot, w *plot.Plot, d *plot.Plot, q *plot.Plot) {
 		fmt.Println("Error during saving the plot:", err)
 	} else {
 		fmt.Println("Plot has been saved to the file 'balloonSurface_simulation.png'.")
+	}
+
+	if err := t.Save(6*vg.Inch, 4*vg.Inch, "balloonSurfaceTension_simulation.png"); err != nil {
+		fmt.Println("Error during saving the plot:", err)
+	} else {
+		fmt.Println("Plot has been saved to the file 'balloonSurfaceTension_simulation.png'.")
 	}
 }
